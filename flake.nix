ef01855ee841c1825176f4474f68e5bbe42ceb9c
Bastian Blokland <mail@bastian.tech>
2026-03-27 18:39:50 +0200

    zed: Indent with 4 spaces

--- flake.nix
+++ flake.nix
@@ -6,29 +6,30 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    pkgsUnstable = import nixpkgs-unstable { inherit system; };
-  in
-  {
-    packages.${system}.default = pkgs.buildDotnetModule {
-      pname = "sharpray";
-      version = "0.1.0";
-      src = ./.;
-      projectFile = "sharpray.csproj";
-      nugetDeps = [ ];
-      dotnet-sdk = pkgs.dotnet-sdk;
-      dotnet-runtime = pkgs.dotnet-runtime;
-    };
+  outputs =
+    { nixpkgs, nixpkgs-unstable, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      pkgsUnstable = import nixpkgs-unstable { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.buildDotnetModule {
+        pname = "sharpray";
+        version = "0.1.0";
+        src = ./.;
+        projectFile = "sharpray.csproj";
+        nugetDeps = [ ];
+        dotnet-sdk = pkgs.dotnet-sdk;
+        dotnet-runtime = pkgs.dotnet-runtime;
+      };
 
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        dotnet-sdk
-        pkgsUnstable.netcoredbg # .NET debug adapter.
-        omnisharp-roslyn
-      ];
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          dotnet-sdk
+          pkgsUnstable.netcoredbg # .NET debug adapter.
+          omnisharp-roslyn
+        ];
+      };
     };
-  };
 }
