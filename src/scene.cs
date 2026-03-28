5ea2ec4ac9d0422f199521168d6ea2352afb0fe7
Bastian Blokland <mail@bastian.tech>
2026-03-28 14:54:02 +0200

    scene: Add sun to the sky

--- src/scene.cs
+++ src/scene.cs
@@ -52,6 +52,10 @@ class Scene
     static readonly Color _skyRadianceMiddle = new Color(1.0f, 0.9f, 0.9f);
     static readonly Color _skyRadianceBottom = new Color(0.5f, 0.425f, 0.275f);
 
+    static readonly Vec3 _sunDir = new Vec3(0.4f, 0.5f, 1f).Normalize();
+    static readonly Color _sunRadiance = new Color(4f, 3.5f, 2.5f);
+    const float _sunCosAngle = 0.999f; // ~2.6 degrees.
+
     public void AddObject(Object obj) => _objects.Add(obj);
 
     public TraceResult Trace(Ray ray, ref Rng rng)
@@ -78,6 +82,11 @@ class Scene
         float topBlend = 1f - MathF.Pow(MathF.Min(1f, 1f + bias - ray.Dir.Y), 4f);
         float bottomBlend = 1f - MathF.Pow(MathF.Min(1f, 1f + bias + ray.Dir.Y), 40f);
         float middleBlend = 1f - topBlend - bottomBlend;
-        return _skyRadianceTop * topBlend + _skyRadianceMiddle * middleBlend + _skyRadianceBottom * bottomBlend;
+        Color sky = _skyRadianceTop * topBlend + _skyRadianceMiddle * middleBlend + _skyRadianceBottom * bottomBlend;
+
+        float sunDot = Vec3.Dot(ray.Dir, _sunDir);
+        float sunBlend = MathF.Max(0f, (sunDot - _sunCosAngle) / (1f - _sunCosAngle));
+
+        return sky + _sunRadiance * sunBlend;
     }
 }
