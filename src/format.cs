468047c3d339fc9399002034b14b0f0f91d3c173
Bastian Blokland <mail@bastian.tech>
2026-04-01 21:04:50 +0300

    info: Move InfoWriter to format.cs

--- src/format.cs
+++ src/format.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Diagnostics;
 using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 readonly struct FormatNum(long n) : ISpanFormattable
 {
@@ -20,6 +23,113 @@ readonly struct FormatMem(long n) : ISpanFormattable
     public bool TryFormat(Span<char> dest, out int written, ReadOnlySpan<char> format, IFormatProvider? provider) =>
         FormatUtils.FormatMem(dest, out written, n);
 }
+
+struct FormatWriter
+{
+    [InterpolatedStringHandler]
+    public struct Formatter
+    {
+        private StringBuilder _sb;
+
+        public Formatter(int literalLength, int formattedCount, FormatWriter writer)
+        {
+            _sb = writer._sb;
+            _sb.Append(' ', writer.Indent * 2);
+        }
+
+        public void AppendLiteral(string s) => _sb.Append(s);
+
+        public void AppendFormatted(string value, int alignment)
+        {
+            int pad = Math.Abs(alignment) - value.Length;
+            if (alignment > 0)
+                _sb.Append(' ', Math.Max(0, pad));
+            _sb.Append(value);
+            if (alignment < 0)
+                _sb.Append(' ', Math.Max(0, pad));
+        }
+
+        public void AppendFormatted<T>(T value)
+        {
+            if (value is ISpanFormattable spanFormattable)
+            {
+                Span<char> buffer = stackalloc char[64];
+                spanFormattable.TryFormat(buffer, out int written, default, CultureInfo.InvariantCulture);
+                _sb.Append(buffer[..written]);
+            }
+            else
+            {
+                _sb.Append(value);
+            }
+        }
+
+        public void AppendFormatted<T>(T value, string format) where T : IFormattable
+        {
+            if (value is ISpanFormattable spanFormattable)
+            {
+                Span<char> buffer = stackalloc char[64];
+                spanFormattable.TryFormat(buffer, out int written, format, CultureInfo.InvariantCulture);
+                _sb.Append(buffer[..written]);
+            }
+            else
+            {
+                _sb.Append(value.ToString(format, CultureInfo.InvariantCulture));
+            }
+        }
+    }
+
+    public int Indent { get; private set; }
+
+    private StringBuilder _sb;
+
+    public FormatWriter(int indent = 0)
+    {
+        Indent = indent;
+        _sb = new StringBuilder();
+    }
+
+    public void Clear()
+    {
+        Indent = 0;
+        _sb.Clear();
+    }
+
+    public void IndentPush() => ++Indent;
+    public void IndentPop()
+    {
+        Debug.Assert(Indent > 0, "Indent underflow");
+        --Indent;
+    }
+
+    public void Write(string text) => _sb.Append(text);
+    public void Write([InterpolatedStringHandlerArgument("")] ref Formatter f) { }
+
+    public void WriteLine(string text) => _sb.AppendLine(new string(' ', Indent * 2) + text);
+    public void WriteLine([InterpolatedStringHandlerArgument("")] ref Formatter f) => _sb.AppendLine();
+
+    public void Separate(int lines = 1)
+    {
+        int remainingNewlines = lines + ((_sb.Length == 0) ? 0 : 1);
+        for (int i = _sb.Length; i-- != 0;)
+        {
+            switch (_sb[i])
+            {
+                case '\r':
+                    continue;
+                case '\n':
+                    if (--remainingNewlines == 0)
+                        return;
+                    continue;
+            }
+            break;
+        }
+        while (remainingNewlines-- != 0)
+            _sb.AppendLine();
+    }
+
+    public override string ToString() => _sb.ToString();
+}
+
 
 static class FormatUtils
 {
